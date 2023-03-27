476ba892fbc29651313e25586e073da1c0962492
keviljp <jk15cohome@gmail.com>
2023-03-26 23:05:41 -0500

    Update Python ODE PBL 2.py

--- Python ODE PBL 2.py
+++ Python ODE PBL 2.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 from scipy.optimize import fsolve
 from scipy import optimize
 from scipy import interpolate
-
-
+import pandas as pd
 
 def odes (x, t):
     #constants
-    c =  .000033 
-    m = .015 #kg
+    #c =  10.09e-4 
+    c =  7.4e-4#kg/m
+    m = .025 #kg
     g = 9.81
     X = 0
     
@@ -27,13 +27,13 @@ def odes (x, t):
     return[dVxdt, dVydt, dYdt]
 
 #define initial conditions for ODEs
-x0 = [253, 0, 1]
+x0 = [14.61, 3.812, 2.01]
 
 # test defined ODEs
 #print(odes(x=x0,t=0))
 
 #declare a time vecotr
-t = np.linspace(0,.5,1000)
+t = np.linspace(0,1.25,1000)
 x = odeint(odes,x0,t)
 
 Vx = x[:,0]
@@ -57,21 +57,27 @@ plt.plot(t, Vy)
 plt.show()
 
 
+plt.xlim(0, 1.3) 
+plt.ylim(0, 3) 
 plt.title('Y Position vs. Time')
 plt.xlabel('Time (s)')
 plt.ylabel('Y Position (m)')
 plt.plot(t, Y)
-plt.axhline(y=0, color='r', linestyle='-')
+#plt.axhline(y=0, color='r', linestyle='-')
+plt.show()
+
+x = Vx*t
+
+plt.xlim(0, 12) 
+plt.ylim(0, 3) 
+plt.title('Y Position vs. X Position')
+plt.xlabel('X position (m)')
+plt.ylabel('Y Position (m)')
+#plt.axhline(y=0, color='r', linestyle='-')
+plt.plot(x, Y)
 plt.show()
 
 f = interpolate.interp1d (t, Y)
 y = interpolate.interp1d (t, Vy)
-root = optimize.newton(f,.46, y)
+root = optimize.newton(f,1.2, y)
 print(root)
-
-print(Y)
-#np.where(Y == 0)[0]
-#f = interpolate.interp1d(t,Y)
-#y = interpolate.interp1d(t,X+1)
-#root = 
-#print(root)
